a088788d70f3fc39c8148a3b77a6af20572dd519
Sebastien Coponat <talent@coponatrecords.com>
2026-02-07 19:52:57 +0100

    new header and hero

--- apps/web/app/[locale]/login/login-client.tsx
+++ apps/web/app/[locale]/login/login-client.tsx
@@ -1,13 +1,13 @@
-"use client";
+// "use client";
 
-import { signIn } from "next-auth/react";
+// import { signIn } from "next-auth/react";
 
-export default function LoginClient() {
-  return (
-    <div>
-      <button onClick={() => signIn("google", { callbackUrl: "/" })}>
-        Sign in with Google
-      </button>
-    </div>
-  );
-}
+// export default function LoginClient() {
+//   return (
+//     <div>
+//       <button onClick={() => signIn("google", { callbackUrl: "/" })}>
+//         Sign in with Google
+//       </button>
+//     </div>
+//   );
+// }
